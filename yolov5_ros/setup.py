e056ee6cfcd912aab8ae7bec84c3a375d556937f
Scottcandy34 <bzbrandonz@gmail.com>
2026-04-01 11:13:35 -0400

    Adds YOLOv5 and bbox_ex_msgs submodules
    Integrates YOLOv5 and bbox_ex_msgs as Git submodules to manage dependencies.
    
    Updates `setup.py` to include package data, ensuring submodule contents are properly installed. Refactors the `sys.path` modification for YOLOv5 in `main.py` to robustly discover the submodule across different installation types.
    
    Removes old YOLOv5 in the repo.

--- yolov5_ros/setup.py
+++ yolov5_ros/setup.py
@@ -11,6 +11,7 @@ setup(
     name=package_name,
     version='0.2.0',
     packages=find_packages(),
+    include_package_data=True,
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
